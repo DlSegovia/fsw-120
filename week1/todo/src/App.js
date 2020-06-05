648fb8d780fcf10210167d6ab355158bd2a69029
DlSegovia <daniel.segovia@bryanuniversity.edu>
2020-06-05 15:22:08 -0500

    assignment 1 todo list

---
+++ week1/todo/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+function App() {
+return (
+  <div>
+    
+    <ul>
+    <input type="chechbox"/>
+      <li>Walk the dog</li>
+      <input type="chechbox"/>
+      <li>Feed the birds</li>
+      <input type="chechbox"/>
+      <li>Change birds cage</li>
+      <input type="chechbox"/>
+      <li>Start School</li>
+    </ul>
+  </div>
+)
+}
+
+export default App
