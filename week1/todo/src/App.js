d17a6711d861ede46b3ab6a9b89d7c278f2557d1
DlSegovia <daniel.segovia@bryanuniversity.edu>
2020-06-07 17:53:01 -0500

    resumit assignment 1 week1

--- week1/todo/src/App.js
+++ week1/todo/src/App.js
@@ -1,18 +1,21 @@
 import React from 'react';
 
 function App() {
-return (
-  <div>
-    
-    <ul>
-    <input type="chechbox"/>
-      <li>Walk the dog</li>
-      <input type="chechbox"/>
-      <li>Feed the birds</li>
-      <input type="chechbox"/>
-      <li>Change birds cage</li>
-      <input type="chechbox"/>
-      <li>Start School</li>
+  return (
+    <div>
+      
+      <ul>
+        <input type="checkbox"/>
+        <li>Walk the dog</li>
+
+        <input type="checkbox"/>
+        <li>Feed the birds</li>
+
+        <input type="checkbox"/>
+        <li>Change birds cage</li>
+        
+        <input type="checkbox"/>
+        <li>Start School</li>
     </ul>
   </div>
 )
