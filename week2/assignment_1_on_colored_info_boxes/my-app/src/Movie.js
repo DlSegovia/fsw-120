ea42dae857a9de1e11c271258e68e670eda2449d
DlSegovia <daniel.segovia@bryanuniversity.edu>
2020-06-13 23:06:18 -0500

    colored info boxes

--- week2/assignment_1_on_colored_info_boxes/my-app/src/Movie.js
+++ week2/assignment_1_on_colored_info_boxes/my-app/src/Movie.js
@@ -1,11 +1,13 @@
 import React from "react"
 
+
 function Movie(props) {
     return (
-        
-        <div style = {{backgroundColor: props.backgroundColor}}>
-            <h3>Title: {props.title}</h3>
-            <h3>Subtitle: {props.subtitle}</h3>
+
+        <div style = {{backgroundColor: props.backgroundColor, width: 500, position: "relative", left: 400}}>
+            <h1>Title: {props.title}</h1>
+            <h2>Stars: {props.stars}</h2>
+            <h2>Release Date: {props.releaseDate}</h2>
             <h3>Information: {props.information}</h3>
             <hr/>
         </div>
