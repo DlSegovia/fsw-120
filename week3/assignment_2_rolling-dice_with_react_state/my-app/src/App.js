83546ce7a51ba642e5246234e9eacc39fa33c1f6
DlSegovia <daniel.segovia@bryanuniversity.edu>
2020-06-24 21:11:51 -0500

    update

--- week3/assignment_2_rolling-dice_with_react_state/my-app/src/App.js
+++ week3/assignment_2_rolling-dice_with_react_state/my-app/src/App.js
@@ -1,95 +1,80 @@
-import React from "react";
-// import one from "./dice_pics/1.png";
-// import two from "./dice_pics/2.png";
-// import three from "./dice_pics/3.png";
-// import four from "./dice_pics/4.png";
-// import five from "./dice_pics/5.png";
-// import six from "./dice_pics/6.png";
-// import "./App.css";
-
-
-class App extends React.Component {
+import React, { Component } from "react"
+import "./App.css";
+class App extends Component {
   constructor() {
     super()
     this.state = {
-      numberOfDice: null,
+      numberOfDice: 0,
       rolls: [],
-      rollSum: null,
-
-    };
-   }
-
-   render() {
-    const DiceItem = this.state.rolls.map((roll, index) => <DiceImage roll={roll} key={index} 
-    const DiceImage = ({ roll }) => {
-      if (roll === 1) 
-      {return <img className="dice-image" 
-        src={"https://raw.githubusercontent.com/DlSegovia/fsw-120/master/week3/assignment_2_rolling-dice_with_react_state/my-app/src/dice_pics/1.png"}
-        alt="1" />;
-      } 
-      else if (roll === 2) 
-      {return <img className="dice-image" 
-      src={"https://raw.githubusercontent.com/DlSegovia/fsw-120/master/week3/assignment_2_rolling-dice_with_react_state/my-app/src/dice_pics/2.png"} alt="2" />;
-      }
-       else if (roll === 3) 
-       {return <img className="dice-image" 
-        src={"https://raw.githubusercontent.com/DlSegovia/fsw-120/master/week3/assignment_2_rolling-dice_with_react_state/my-app/src/dice_pics/3.png"} alt="3" />;
-      }
-       else if (roll === 4) 
-       {return <img className="dice-image" 
-      src={"https://raw.githubusercontent.com/DlSegovia/fsw-120/master/week3/assignment_2_rolling-dice_with_react_state/my-app/src/dice_pics/4.png"} alt="4" />;
-      }
-       else if (roll === 5) 
-       {return <img className="dice-image" 
-      src={"https://raw.githubusercontent.com/DlSegovia/fsw-120/master/week3/assignment_2_rolling-dice_with_react_state/my-app/src/dice_pics/5.png"} alt="5" />;
-      }
-       else if (roll === 6) 
-       {return <img className="dice-image" 
-      src={"https://raw.githubusercontent.com/DlSegovia/fsw-120/master/week3/assignment_2_rolling-dice_with_react_state/my-app/src/dice_pics/6.png"} alt="6" />;
-      }
+      rollSum: 0
     }
-    
-         
+  }
+  diceRoll = numberOfDice => {
 
-    const diceRoll = numberOfDice => {
-      let rolls = [];
-      let rollSum = 0;
-      for (let i = 0; i < numberOfDice; i++) {
-        rolls[i] = Math.floor(Math.random() * 6) + 1;
-        rollSum += rolls[i];
-      }
-      this.setState({
-        numberOfDice,
-        rolls,
-        rollSum
-      })
+    let rolls = [];
+    let rollSum = 0;
+    for (let i = 0; i < numberOfDice; i++) {
+
+      rolls[i] = Math.floor(Math.random() * 6) + 1;
+      console.log(numberOfDice, "numberOfDice")
+      console.log(rolls, "rolls")
+      rollSum += rolls[i];
+      console.log(rollSum, "rollSum")
     }
-    
-  }/>)
-  
+    this.setState({
+      numberOfDice,
+      rolls,
+      rollSum
+    })
+  }
+  render() {
     return (
-    <div className="DiceContainer">
-        <h1 className="header">Try Your Luck and Roll The Dice</h1>
+      <div className="App">
+        <h1>Dice Roll Demo</h1>
+        {[1, 2, 3, 4, 5].map(number => {
+          let text = number === 1 ? "die" : "dice";
+          return (
+            <button
+              key={number}
+              onClick={() => this.diceRoll(number)}
+              className="button">
+              {number} {text}
+            </button>
+          );
+        })}
+        <p>{this.state.rolls.map((roll, index) => {
+          //  const DiceImage = ( {roll} ) => {
+          if (roll === 1) {
+            return <img className="dice-image"
+              src={"https://raw.githubusercontent.com/DlSegovia/fsw-120/master/week3/assignment_2_rolling-dice_with_react_state/my-app/src/dice_pics/1.png"}
+              alt="1" />;
+          }
+          else if (roll === 2) {
+            return <img className="dice-image"
+              src={"https://raw.githubusercontent.com/DlSegovia/fsw-120/master/week3/assignment_2_rolling-dice_with_react_state/my-app/src/dice_pics/2.png"} alt="2" />;
+          }
+          else if (roll === 3) {
+            return <img className="dice-image"
+              src={"https://raw.githubusercontent.com/DlSegovia/fsw-120/master/week3/assignment_2_rolling-dice_with_react_state/my-app/src/dice_pics/3.png"} alt="3" />;
+          }
+          else if (roll === 4) {
+            return <img className="dice-image"
+              src={"https://raw.githubusercontent.com/DlSegovia/fsw-120/master/week3/assignment_2_rolling-dice_with_react_state/my-app/src/dice_pics/4.png"} alt="4" />;
+          }
+          else if (roll === 5) {
+            return <img className="dice-image"
+              src={"https://raw.githubusercontent.com/DlSegovia/fsw-120/master/week3/assignment_2_rolling-dice_with_react_state/my-app/src/dice_pics/5.png"} alt="5" />;
+          }
+          else if (roll === 6) {
+            return <img className="dice-image"
+              src={"https://raw.githubusercontent.com/DlSegovia/fsw-120/master/week3/assignment_2_rolling-dice_with_react_state/my-app/src/dice_pics/6.png"} alt="6" />;
+          }
+        }
+        )}</p>
+      </div>
+    )
+  }
+}
 
-        <div className="buttons">
-        {
-         
-        };
-  {[1, 2, 3, 4, 5].map(number => {
-    let text = number === 1 ? "die" : "dice";
-    return (
-      <button
-        key={number}
-        onClick={() => this.diceRoll(number)}
-        className="button"
-      >
-        {number} {text}
-      </button>
-      
-    );
-  })}
-</div>
-    </div>
-  )
 
-  export default App
+export default App
--- week3/assignment_3_dj_react/my-app/src/App.js
+++ week3/assignment_3_dj_react/my-app/src/App.js
@@ -1,33 +1,42 @@
 import React from "react";
+import ReactDOM from 'react-dom';
 import "./App.css"
 
-class App extends React.createClass({
-  constructor() {
-    super()
-    this.state = {
-          color: 'white'
-      }
-      this.handleClick = this.handleClick.bind(this)
-  }
+class App extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            color: 'black'
+        }}
+        var Box = React.createClass({
+            getInitialState: function() {
+                return {
+                    color: 'black'
+                };
+            },
+    changeColor: function() {
+        var newColor = this.state.color === 'white' ? 'black' : 'white';
+        this.setState({
+            color: newColor
+        });
+    },
 
-  changeColor: function() {
-    var newColor = this.state.color === 'white' ? 'black' : 'white';
-    this.setState({ color: newColor });
-},
+    render: function() {
+        return (
+            <div>
+                <div
+                    style = {{
+                        background: this.state.color,
+                        width: 100,
+                        height: 100
+                    }}
+                    onClick = {this.changeColor}
+                >
+                </div>
+            </div>
+        )},
 
-render: function() {
-  return (
-      <div>
-          <div
-              className='box'
-              style={{background:this.state.color}}
-              onClick={this.changeColor}
-          >
-              In here already
-          </div>
-      </div>
-  )
-}
-})
+
+
 
 export default App
