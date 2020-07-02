c3a5c4705142048a04c69d041c7fe72e3442d705
DlSegovia <daniel.segovia@bryanuniversity.edu>
2020-07-02 04:58:05 -0500

    update

--- week5/assignment_1_react_roto_router/my-app/src/components/Home.js
+++ week5/assignment_1_react_roto_router/my-app/src/components/Home.js
@@ -3,8 +3,13 @@ import React from 'react';
 function Home() {
   return (
     <div>
-      Home
-      
+      <img className='image1' src='https://github.com/DlSegovia/fsw-120/blob/master/week5/assignment_1_react_roto_router/my-app/src/Roto%20Router/when%20you%20cant%20do%20it%20yoursefl%20leak-plumber.jpg?raw=true'/>
+    <p className='p1'>
+When you can't handle it yourself!!!
+    </p>
+    <p className='p2'>
+      Call A Plumber...
+    </p>
     </div>
   );
 }
--- week5/assignment_1_react_roto_router/my-app/src/components/Navbar.js
+++ week5/assignment_1_react_roto_router/my-app/src/components/Navbar.js
@@ -4,10 +4,10 @@ import { Link } from 'react-router-dom'
 
 function Navbar() {
   return (
+    
     <div className="Navbar">
-      
-<img/>
       <ul className='my-link-wrapper'>
+    <img src= 'https://github.com/DlSegovia/fsw-120/blob/master/week5/assignment_1_react_roto_router/my-app/src/Roto%20Router/logo.png?raw=true'/>
           <Link className='my-nav-links' to='/'> Home </Link>
           <Link className='my-nav-links' to='/about'> About </Link>
           <Link className='my-nav-links' to='/register'> Register </Link>
