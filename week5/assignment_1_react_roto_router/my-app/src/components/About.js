d0704d6d4195b6e20f53b369c0cb2e048eb42421
DlSegovia <daniel.segovia@bryanuniversity.edu>
2020-07-03 23:42:12 -0500

    update image

--- week5/assignment_1_react_roto_router/my-app/src/components/About.js
+++ week5/assignment_1_react_roto_router/my-app/src/components/About.js
@@ -4,10 +4,13 @@ function About() {
   return (
     <div className="About">
       <div>
-<img src= '' atl=''/>
+<img className='image2' src= 'https://github.com/DlSegovia/fsw-120/blob/master/week5/assignment_1_react_roto_router/my-app/src/Roto%20Router/plumber.jpg?raw=true' atl=''/>
       </div>
       <div>
-<img />
+        <img className='image3' src='' alt=' '/>
+      </div>
+      <div>
+<img className='image3' src= 'https://github.com/DlSegovia/fsw-120/blob/master/week5/assignment_1_react_roto_router/my-app/src/Roto%20Router/woman%20plumber.jpg?raw=true' alt='' />
       </div>
     </div>
   );
