b31e372164b11735eed6bf8ba618dcccda884f97
DlSegovia <daniel.segovia@bryanuniversity.edu>
2020-07-05 01:43:47 -0500

    assignment 1 react roto router

--- week5/assignment_1_react_roto_router/my-app/src/components/Home.js
+++ week5/assignment_1_react_roto_router/my-app/src/components/Home.js
@@ -7,12 +7,19 @@ function Home() {
       <img className='image1' src='https://github.com/DlSegovia/fsw-120/blob/master/week5/assignment_1_react_roto_router/my-app/src/Roto%20Router/when%20you%20cant%20do%20it%20yoursefl%20leak-plumber.jpg?raw=true'alt=" "/>
     </div>
     <p className='footer'>
-Footer!!!
+      <img className='ftLogo' src="https://github.com/DlSegovia/fsw-120/blob/master/week5/assignment_1_react_roto_router/my-app/src/Roto%20Router/logo.png?raw=true" alt=""/>
+    <img className='facebook' src="https://raw.githubusercontent.com/DlSegovia/fsw-120/2e6b01acae7b195516c62e7f5baa1eb3a78ed501/week5/assignment_1_react_roto_router/my-app/src/Roto%20Router/facebook.svg" alt=""/>
+    <img className='intGram' src="https://raw.githubusercontent.com/DlSegovia/fsw-120/2e6b01acae7b195516c62e7f5baa1eb3a78ed501/week5/assignment_1_react_roto_router/my-app/src/Roto%20Router/instagram-sketched.svg" alt=""/>
+    <img className='youtube' src="https://raw.githubusercontent.com/DlSegovia/fsw-120/2e6b01acae7b195516c62e7f5baa1eb3a78ed501/week5/assignment_1_react_roto_router/my-app/src/Roto%20Router/youtube.svg" alt=""/>
+    <img className='linkedin' src="https://raw.githubusercontent.com/DlSegovia/fsw-120/2e6b01acae7b195516c62e7f5baa1eb3a78ed501/week5/assignment_1_react_roto_router/my-app/src/Roto%20Router/linkedin.svg" alt=""/>
+    <img className='twitter' src="https://raw.githubusercontent.com/DlSegovia/fsw-120/2e6b01acae7b195516c62e7f5baa1eb3a78ed501/week5/assignment_1_react_roto_router/my-app/src/Roto%20Router/twitter.svg" alt=""/>
+    
+    <p className='someAdd'>Plumbing Services<br />
+        72020 Some St.<br />
+        Somewhere, In The World 78946<br />
+        Phone: (123)567-9840
     </p>
-    <span>
-    <i className="fa fa-credit-card" aria-hidden="true"></i>
-    <i class="fa fa-facebook-official" aria-hidden="true"></i>
-    </span>
+    </p>
     </div>
   );
 }
