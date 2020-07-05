2e6b01acae7b195516c62e7f5baa1eb3a78ed501
DlSegovia <daniel.segovia@bryanuniversity.edu>
2020-07-05 00:34:53 -0500

    icons

--- week5/assignment_1_react_roto_router/my-app/src/components/Home.js
+++ week5/assignment_1_react_roto_router/my-app/src/components/Home.js
@@ -6,12 +6,13 @@ function Home() {
       <div>
       <img className='image1' src='https://github.com/DlSegovia/fsw-120/blob/master/week5/assignment_1_react_roto_router/my-app/src/Roto%20Router/when%20you%20cant%20do%20it%20yoursefl%20leak-plumber.jpg?raw=true'alt=" "/>
     </div>
-    <p className='p1'>
-When you can't handle it yourself!!!
+    <p className='footer'>
+Footer!!!
     </p>
-    <p className='p2'>
-      Call A Plumber...
-    </p>
+    <span>
+    <i className="fa fa-credit-card" aria-hidden="true"></i>
+    <i class="fa fa-facebook-official" aria-hidden="true"></i>
+    </span>
     </div>
   );
 }
