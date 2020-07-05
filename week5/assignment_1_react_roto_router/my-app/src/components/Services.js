7afef57cd825707e75c1cd414358ab388eb4d321
DlSegovia <daniel.segovia@bryanuniversity.edu>
2020-07-04 22:22:29 -0500

    image update

--- week5/assignment_1_react_roto_router/my-app/src/components/Services.js
+++ week5/assignment_1_react_roto_router/my-app/src/components/Services.js
@@ -4,8 +4,26 @@ function Services() {
   return (
 
     <div>
+      <img className='image5' src='https://github.com/DlSegovia/fsw-120/blob/master/week5/assignment_1_react_roto_router/my-app/src/Roto%20Router/Untitled14.jpg?raw=true' alt='' />
+
+      <img className='image6' src='https://github.com/DlSegovia/fsw-120/blob/master/week5/assignment_1_react_roto_router/my-app/src/Roto%20Router/Untitled13.jpg?raw=true' alt='' />
+
+      <img className='image7' src='https://github.com/DlSegovia/fsw-120/blob/master/week5/assignment_1_react_roto_router/my-app/src/Roto%20Router/Untitled7.jpg?raw=true' alt='' />
+
+      <img className='image8' src='https://github.com/DlSegovia/fsw-120/blob/master/week5/assignment_1_react_roto_router/my-app/src/Roto%20Router/Untitled11.jpg?raw=true' alt='' />
+
+      <img className='image9' src='https://github.com/DlSegovia/fsw-120/blob/master/week5/assignment_1_react_roto_router/my-app/src/Roto%20Router/Untitled5.jpg?raw=true' alt='' />
+
+      <img className='image10' src='https://github.com/DlSegovia/fsw-120/blob/master/week5/assignment_1_react_roto_router/my-app/src/Roto%20Router/Untitled12.jpg?raw=true' alt=' ' />
+
+      <img className='image11' src='https://github.com/DlSegovia/fsw-120/blob/master/week5/assignment_1_react_roto_router/my-app/src/Roto%20Router/Untitled6.jpg?raw=true' alt=' ' />
+
+      <img className='image12' src='https://github.com/DlSegovia/fsw-120/blob/master/week5/assignment_1_react_roto_router/my-app/src/Roto%20Router/Untitled10.jpg?raw=true' alt=' ' />
+
+      <img className='image13' src='https://github.com/DlSegovia/fsw-120/blob/master/week5/assignment_1_react_roto_router/my-app/src/Roto%20Router/Untitled3.jpg?raw=true' alt=' ' />
 
     </div>
+
   )
 }
 
